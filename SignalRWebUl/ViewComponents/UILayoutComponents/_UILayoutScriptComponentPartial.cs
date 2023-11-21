709fc8be80e1ecd6b8dd65e26747fd8962f08d48
Cem Koç <cem-koc@outlook.com>
2023-11-21 17:58:45 +0300

    UI Tasarımı

---
+++ SignalRWebUl/ViewComponents/UILayoutComponents/_UILayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRWebUl.ViewComponents.UILayoutComponents
+{
+    public class _UILayoutScriptComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
