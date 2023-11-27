73e4a58611d9f7eb295826228740ed2dbda1be2f
Cem Koç <cem-koc@outlook.com>
2023-11-27 21:39:54 +0300

    Revizeler

--- SignalR.DtoLayer/SliderDto/ResultSliderDto.cs
+++ SignalR.DtoLayer/SliderDto/ResultSliderDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SignalRWebUl.Dtos.SliderDtos
+namespace SignalR.DtoLayer.SliderDto
 {
 	public class ResultSliderDto
 	{
--- SignalRWebUl/Controllers/BookingController.cs
+++ SignalRWebUl/Controllers/BookingController.cs
@@ -36,6 +36,7 @@ namespace SignalRWebUl.Controllers
 		[HttpPost]
 		public async Task<IActionResult> CreateBooking(CreateBookingDto createBookingDto)
 		{
+			createBookingDto.Description = "Rezervasyon Alındı";
 			var client = _httpClientFactory.CreateClient();
 			var jsonDate = JsonConvert.SerializeObject(createBookingDto);
 			StringContent stringContent = new StringContent(jsonDate, Encoding.UTF8, "application/json");
@@ -84,6 +85,20 @@ namespace SignalRWebUl.Controllers
 				return RedirectToAction("Index");
 			}
 			return View();
+		}
+
+		public async Task<IActionResult> BookingStatusApproved(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7114/api/Booking/BookingStatusApproved/{id}");
+			return RedirectToAction("Index");
+		}
+
+		public async Task<IActionResult> BookingStatusCancelled(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			await client.GetAsync($"https://localhost:7114/api/Booking/BookingStatusCancelled/{id}");
+			return RedirectToAction("Index");
 		}
 	}
 }
